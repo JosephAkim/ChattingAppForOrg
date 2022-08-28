233c0a634930c4f5329c6f7785b0f7f10be8a2e7
JosephAkim <Udokimjoseph@gmail.com>
2022-08-28 04:18:12 +0100

    Start work

--- app/src/main/java/com/kim/chattingappfororg/activities/MainActivity.java
+++ app/src/main/java/com/kim/chattingappfororg/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.kim.chattingappfororg;
+package com.kim.chattingappfororg.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.kim.chattingappfororg.R;
 
 public class MainActivity extends AppCompatActivity {
 
